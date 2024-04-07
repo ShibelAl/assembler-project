fcc5c45d5755000166049f3a63dde045888723f8
ShibelAl <shibelalshech300@gmail.com>
2024-04-07 23:47:49 +0300

    changed the funcionality of main function and improved the code

--- assembler.c
+++ assembler.c
@@ -2,71 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include "defines.h"
+#include "preprocessor.h"
+#include "first_pass.h"
 
 
 int main(int argc, char *argv[]){
+
 	FILE *input_fp, *output_fp; /*fp: file pointer*/
-	/*FILE *am_fp;*/
-	char *file_name;
-	int file_exist;
+	char *as_file; /*as_file refers to the source file, that ends with .as*/
+	char *am_file; /*am_file refers to the new file generated after macro expantion*/
     
 	/*if the user didn't provide any file*/
 	if(argc == 1){
-		printf("\nerror - there is no files!\n");
+		printf("error - there is no files!\n");
 		return 0;
 	}
-
+	
 	while(--argc > 0){
-	
-		file_exist = TRUE;
 		
-		file_name = (char *)malloc(strlen(*argv));
-		if(file_name == NULL){
-			printf("\nmemory allocation failed, cannot open file.\n");
+		argv++;
+		
+		as_file = (char *)malloc(strlen(*argv));
+		am_file = (char *)malloc(strlen(*argv));
+		if(as_file == NULL || am_file == NULL){
+			printf("memory allocation failed, cannot open file.\n");
 			exit(1);
 		}
-
-		strcpy(file_name, *++argv);
-		strcat(file_name, ".as");      
-	
-		/*open file for reading*/
-		if(((input_fp = fopen(file_name, "r")) == NULL) && (argc > 1)){
-			printf("\ncannot open file %s, continue with next file.\n\n", *argv);
-			file_exist = FALSE;
+		
+		strcpy(as_file, *argv);
+		strcpy(am_file, *argv);
+		
+		strcat(as_file, ".as");
+		strcat(am_file, ".am");
+		
+		input_fp = fopen(as_file, "r");
+		output_fp = fopen(am_file, "w+");
+		
+		if(input_fp == NULL){
+			printf("cannot open file %s.\n", as_file);
+			continue;	
 		}
 		
-		else if(((input_fp = fopen(file_name, "r")) == NULL) && (argc == 1)){
-			printf("\ncannot open file %s.\n\n", *argv);
-			exit(1);
+		if(output_fp == NULL){
+			printf("cannot open file %s.\n", am_file);
+			continue;
 		}
-	
-	
-		if(file_exist){
-			/*creating a new file with the extention ".am"*/
-			strcpy(file_name, strtok(file_name, "."));
-			strcat(file_name, ".am");
-
-			/*open file for writing*/
-			if(((output_fp = fopen(file_name, "w")) == NULL) && (argc > 1))
-				printf("\ncannot open file %s, continue with next file.\n\n", *argv);
-
-			else if(((output_fp = fopen(file_name, "w")) == NULL) && (argc == 1)){
-				printf("\ncannot open file %s.\n\n", *argv);
-				exit(1);
-			}
-
-			/*am_fp = preprocessor(input_fp, output_fp);*/    /*waiting to be used..*/
-			/*to be continued*/
-
-		}
-
+		
+		expand_macro(input_fp, output_fp);
+		fclose(input_fp);
+		
+		rewind(output_fp);
+		first_pass(output_fp);
+		
+		free(as_file);
+		free(am_file);
 	}
      
-	free(file_name);
-	fclose(output_fp);
-	fclose(input_fp);
+	fclose(output_fp);     
      
-     
-return 0;
+	return 0;
 }
 
