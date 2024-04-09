a7ce8418a932023b11e96d58dffc578c30bbbedc
ShibelAl <shibelalshech300@gmail.com>
2024-04-09 22:24:35 +0300

    more defines

--- defines.h
+++ defines.h
@@ -1,4 +1,6 @@
-enum boolean{FALSE, TRUE};
+#define LABEL_LENGTH 31
+#define OPCODE_LENGTH 4
 #define LINE_SIZE 81 /*80 + the character '\n'*/
 #define COMMAND_QTY 16 /*number of commands in the assembly language*/
 
+enum boolean{FALSE, TRUE};
