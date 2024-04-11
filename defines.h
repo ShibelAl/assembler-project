99e7183a0393d043d492e6ab1d4e564e3a960d9d
ShibelAl <shibelalshech300@gmail.com>
2024-04-11 23:30:53 +0300

    defined register quantity

--- defines.h
+++ defines.h
@@ -2,5 +2,6 @@
 #define OPCODE_LENGTH 4
 #define LINE_SIZE 81 /*80 + the character '\n'*/
 #define COMMAND_QTY 16 /*number of commands in the assembly language*/
+#define REGISTER_QTY 8 /*number of registers in the given computer*/
 
 enum boolean{FALSE, TRUE};
