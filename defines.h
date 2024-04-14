e03bb672a76c17c6e9f0b91412a7c40955a71d48
ShibelAl <shibelalshech300@gmail.com>
2024-04-14 23:48:02 +0300

    added defines

--- defines.h
+++ defines.h
@@ -3,5 +3,9 @@
 #define LINE_SIZE 81 /*80 + the character '\n'*/
 #define COMMAND_QTY 16 /*number of commands in the assembly language*/
 #define REGISTER_QTY 8 /*number of registers in the given computer*/
+#define MAX_DIGITS 4 /* Max length of base32 representation of the decimal number, 
+3 characters for the number + 1 for null terminator */
+#define BINARY_LENGTH 11
 
 enum boolean{FALSE, TRUE};
+
